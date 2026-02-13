8d06de84f2f00ce7c4984e166f8d82b5de1f5010
rQxwX3 <github@scrt.simplelogin.com>
2026-02-13 21:25:56 +0300

    Add config.SetUser()

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,16 +6,40 @@ import (
 )
 
 type Config struct {
-	DBurl string `json:"db_url"`
+	DBurl           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+const (
+	configFileName = "/.gatorconfig.json"
+)
+
+func getConfigPath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+
+	if err != nil {
+		return "", err
+	}
+
+	return homeDir + configFileName, nil
+}
+
+func getConfigData() ([]byte, error) {
+	configPath, err := getConfigPath()
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
 
 func Read() (Config, error) {
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		return Config{}, err
-	}
-
-	data, err := os.ReadFile(homeDir + "/.gatorconfig.json")
+	data, err := getConfigData()
 	if err != nil {
 		return Config{}, err
 	}
@@ -26,4 +50,35 @@ func Read() (Config, error) {
 	}
 
 	return config, nil
+}
+
+func SetUser(username string) error {
+	data, err := getConfigData()
+	if err != nil {
+		return err
+	}
+
+	var config Config
+	if err = json.Unmarshal(data, &config); err != nil {
+		return err
+	}
+
+	config.CurrentUserName = username
+
+	data, err = json.Marshal(config)
+	if err != nil {
+		return err
+	}
+
+	configPath, err := getConfigPath()
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(configPath, data, os.ModeAppend)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
